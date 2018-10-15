a9dd28563cc6a5e91178e281a0e9ebceb8b55230
“Dg” <jeong214132@naver.com>
2018-10-15 15:38:21 +0900

    extending schema resolvers

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,15 +1,23 @@
 
 //resolvers.js
-const patrick ={
+const people =[
+{
+    id: 0,
     name:"pat",
     age: 18,
     gender: "male"
-};
+},
+{
+    id: 1,
+    name:"rick",
+    age: 20,
+    gender: "female"
+}];
 
 
 const resolvers ={
     Query: {
-        person: ()=>patrick
+        people: ()=>people
     }
 };
 
